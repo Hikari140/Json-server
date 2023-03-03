bd64a782c4e6f8dd7e01e8cabc8188336d80846e
Isadora Maiolino <maiolinogm@hotmail.com>
2023-03-03 11:51:55 -0300

    Add files via upload

---
+++ json-server/script.js
@@ -0,0 +1,152 @@
+{
+    "Cinema" [
+      {
+        "id": 1,
+        "nome": " Cinemania ",
+        "gerente": " Roberto ",
+        "local": " Rua Carmélia "
+      },
+      {
+        "id": 2,
+        "nome": " Universo dos Filmes ",
+        "gerente": " Carla ",
+        "local": " Rua Dória Silva "
+      },
+      {
+        "id": 3,
+        "nome": " Cine Center ",
+        "gerente": " Monica ",
+        "local": " Rua Carlos de Andrade "
+      },
+      {
+        "id": 4,
+        "nome": " Uniplex ",
+        "gerente": " Igor ",
+        "local": " Rua Jorge Cordeiro "
+      }
+    ],
+    "Horários" [
+      {
+        "id:": 1,
+        "abertura": " 10:00 ",
+        "fechamento": " 22:00 ",
+        "não abre": " Domingo "
+      },
+      {
+        "id:": 2,
+        "abertura": " 10:00 ",
+        "fechamento": " 22:00 ",
+        "não abre": " Domingo "
+      },
+      {
+        "id:": 3,
+        "abertura": " 10:00 ",
+        "fechamento": " 22:00 ",
+        "não abre": " Domingo "
+      },
+      {
+        "id:": 4,
+        "abertura": " 10:00 ",
+        "fechamento": " 22:00 ",
+        "não abre": " Domingo "
+      }
+    ],
+    "Catálogo e Preço" [
+      {
+        "id": 1,
+        "nome": " Avatar 2 ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 03:00 "
+      },
+      {
+        "id": 1,
+        "nome": " Gato de Botas 2 ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:30 "
+      },
+      {
+        "id": 1,
+        "nome": " Harry Potter ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:00 "
+      },
+      {
+        "id": 1,
+        "nome": " Pinóquio ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:30 "
+      },
+      {
+        "id": 1,
+        "nome": " As Branquelas ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:30 "
+      },
+      {
+        "id": 1,
+        "nome": " A Bela e a Fera ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 01:40 "
+      },
+      {
+        "id": 1,
+        "nome": " Shrek ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:10 "
+      },
+      {
+        "id": 1,
+        "nome": " Shrek 2 ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:10 "
+      },
+      {
+        "id": 1,
+        "nome": " Shrek 3 ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:30 "
+      },
+      {
+        "id": 1,
+        "nome": " Shrek Para Sempre ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:40 "
+      },
+      {
+        "id": 1,
+        "nome": " A Pequena Sereia ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:00 "
+      },
+      {
+        "id": 1,
+        "nome": " João e Maria ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:30 "
+      },
+      {
+        "id": 1,
+        "nome": " Hanna Montana ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 01:40 "
+      },
+      {
+        "id": 1,
+        "nome": " Demons Slayer ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:05 "
+      },
+      {
+        "id": 1,
+        "nome": " Tropa de Elite ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:40 "
+      },
+      {
+        "id": 1,
+        "nome": " Tropa de Elite 2 ",
+        "valor": " R$ 15,00 ",
+        "Duração": " 02:40 "
+      }
+    ]
+  }
